6ce6bf915f973588f47d3416d354cd621a98f6c8
simran <simranjitkaur59149@gamil.com>
2026-03-31 18:42:29 +0530

    update server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "blogwebs.vercel.app",
+    origin: "https://blogweb-wheat.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
   }),
